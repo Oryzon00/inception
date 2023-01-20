20142ab00f34a8edc2e3e9e7b73819abb1cc0a59
Adrian Jung <adrian.mael.jung@gmail.com>
2023-01-21 00:47:25 +0100

    go home

--- end.sh
+++ end.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo copy vm from goinfre to sgoinfre
 cp -R /mnt/nfs/homes/ajung/goinfre/inception-graphical	/mnt/nfs/homes/ajung/sgoinfre/
+#/mnt/nfs/homes/ajung/sgoinfre/inception-graphical/Snapshots
--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo delete vm goinfre
+rm -rf /mnt/nfs/homes/ajung/goinfre/inception-graphical
+
+echo copy vm from sgoinfre to goinfre
 cp -R /mnt/nfs/homes/ajung/sgoinfre/inception-graphical	/mnt/nfs/homes/ajung/goinfre
