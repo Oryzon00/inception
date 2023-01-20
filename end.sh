d59171ecc1bd6e062ab880e8f28050eb5eaefefd
Adrian Jung <adrian.mael.jung@gmail.com>
2023-01-21 00:09:11 +0100

    script

---
+++ end.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -R /mnt/nfs/homes/ajung/goinfre/inception-graphical	/mnt/nfs/homes/ajung/sgoinfre/
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -R /mnt/nfs/homes/ajung/sgoinfre/inception-graphical	/mnt/nfs/homes/ajung/goinfre
